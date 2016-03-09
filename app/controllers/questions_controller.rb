c38f8b3e9591122bd0e5b19d4d9ad899e332086a
Mira Scarvalone <mirascarvalone@Miras-MacBook-Pro.local>
2016-03-09 16:22:06 -0500

    Add new question page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,11 +3,28 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
-  def new
-  end
-
   def show
     @question = Question.find(params[:id])
     @answers = Answer.where(question: @question)
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params.merge(user_id: current_user.id))
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :content)
+  end
+
 end

51009c3333df10117909cc64d235cbea0a5b734b
Mira Scarvalone <mirascarvalone@Miras-MacBook-Pro.local>
2016-03-09 14:42:59 -0500

    Edit user table to have password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309191221) do
+ActiveRecord::Schema.define(version: 20160309194118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,10 +44,10 @@ ActiveRecord::Schema.define(version: 20160309191221) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.text     "username",   null: false
-    t.text     "password",   null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "username",        null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   create_table "votes", force: :cascade do |t|

9da5ba5f71f5bc4dc265279d52d9a718266d60fd
Mira Scarvalone <mirascarvalone@Miras-MacBook-Pro.local>
2016-03-09 14:53:58 -0500

    Add login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Thanks #{@user.username}! You successfully registered"
-      redirect to user
+      redirect_to @user
     else
       render :new
     end
   end
 
   def show
-    @user = User.find(:id)
+    @user = User.find(params[:id])
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   has_many :answers
   has_many :votes, class_name: 'Votes', foreign_key: 'voter_id'
   has_many :comments, class_name: 'Votes', foreign_key: 'commenter_id'
+  validates :username, presence: true, uniqueness: true
+
 end

df7b16c02144a2b762280fc52d7eb5478bd09e12
Mira Scarvalone <mirascarvalone@Miras-MacBook-Pro.local>
2016-03-09 15:08:31 -0500

    Add helper log in/out methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,29 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  before_action :ensure_current_user, only: [:new, :create, :update, :destroy]
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def ensure_current_user
+    redirect_to new_session_path unless current_user
+  end
 end

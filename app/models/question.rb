4da0124068857ee0c29e8b53cb5ebcc99f1bbd33
Mira Scarvalone <mirascarvalone@Miras-MacBook-Pro.local>
2016-03-09 14:37:59 -0500

    Add model associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ActiveRecord::Base
+  belongs_to :user
+  has_many :votes, as: :votable
+  has_many :comments, as: :commentable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :questions
+  has_many :answers
+  has_many :votes, class_name: 'Votes', foreign_key: 'voter_id'
+  has_many :comments, class_name: 'Votes', foreign_key: 'commenter_id'
 end

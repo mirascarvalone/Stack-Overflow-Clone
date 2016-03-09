3ff1ae9f2fe3400c610b8e15a548e552c9720ac3
Mira Scarvalone <mirascarvalone@Miras-MacBook-Pro.local>
2016-03-09 16:29:32 -0500

    Add question edit and delete forms

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -21,6 +21,25 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
+  end
+
   private
 
   def question_params

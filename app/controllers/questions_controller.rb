295c507b089b596a4f28ccdd01804fac3937ec05
Mira Scarvalone <mirascarvalone@Miras-MacBook-Pro.local>
2016-04-04 12:40:48 -0400

    Add answer functionality

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,6 +6,7 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
     @answers = Answer.where(question: @question)
+    @answer = Answer.new
   end
 
   def new
